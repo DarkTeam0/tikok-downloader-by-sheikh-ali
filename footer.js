1e178ded5706d15749aa6decde6cfb5d1dcef89a
DarkTeam0 <afaquekhan839@gmail.com>
2025-07-04 23:47:58 +0500

    Update footer.js

--- footer.js
+++ footer.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function() {
             color: white;
         }
         .credit-footer a:hover {
-            transform: scale(0.5);
+            transform: scale(0.2);
             box-shadow: 0 4px 8px rgba(0,255,255,0.4);
         }
     `;

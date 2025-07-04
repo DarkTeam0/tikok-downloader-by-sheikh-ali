192faff069172c311ddef5ac7997146550c032c2
DarkTeam0 <afaquekhan839@gmail.com>
2025-07-05 00:15:45 +0500

    Update footer.js

--- footer.js
+++ footer.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const footer = document.createElement('div');
     footer.innerHTML = `
         <div class="credit-footer">
-            <strong>CODE BY SHEIKH ALI</strong><br><br>
+            <strong>CODE BY 𒁂𓄂❥.𝑺𝑯𝑬𝑰𝑲𝑯 𝑨𝑳𝑰 🔥༽༼ ♡𒁂</strong><br><br>
             <a href="https://whatsapp.com/channel/0029VahpgB7L7UVXkL6oMw1k" target="_blank" class="whatsapp">Join WhatsApp</a>
             <a href="https://t.me/+94FrpOJan0plZGQ0" target="_blank" class="telegram">Join Telegram</a>
         </div>
@@ -17,11 +17,11 @@ document.addEventListener("DOMContentLoaded", function() {
     style.textContent = `
         .credit-footer {
             background: #111;
-            padding: 20px;
+            padding: 14px;
             text-align: center;
             border-top: 2px solid #00bcd4;
             box-shadow: 0 -2px 10px rgba(0,255,255,0.2);
-            font-size: 16px;
+            font-size: 10px;
             margin-top: 40px;
             position: fixed;
             bottom: 0;
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function() {
             color: white;
         }
         .credit-footer a:hover {
-            transform: scale(1);
+            transform: scale(0.9);
             box-shadow: 0 4px 8px rgba(0,255,255,0.4);
         }
     `;
